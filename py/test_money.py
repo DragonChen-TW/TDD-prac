466c843a1f4d64e5f8121f653860040120c6617c
teacher144123 <teacher144123@gmail.com>
2022-08-30 15:22:05 +0800

    feat: multiplication and division done.

--- py/test_money.py
+++ py/test_money.py
@@ -5,20 +5,30 @@ class Money:
         self.amount = amount
         self.currency = currency
     
+    def __eq__(self, other):
+        return self.amount == other.amount and self.currency == other.currency
+    
     def times(self, multiplier):
         return Money(self.amount * multiplier, self.currency)
+    
+    def divide(self, divisor):
+        return Money(self.amount / divisor, self.currency)
 
 class TestMoney(unittest.TestCase):
     def testMultiplicationInDollars(self):
-        fiver = Money(5, "USD")
-        tenner = fiver.times(2)
-        self.assertEqual(tenner.amount, 10)
+        fiveDollars = Money(5, "USD")
+        tenDollars = Money(10, "USD")
+        self.assertEqual(tenDollars, fiveDollars.times(2))
     
     def testMultiplicationEuros(self):
         tenEuros = Money(10, 'EUR')
-        twentyEuros = tenEuros.times(2)
-        self.assertEqual(twentyEuros.amount, 20)
-        self.assertEqual(twentyEuros.currency, 'EUR')
+        twentyEuros = Money(20, 'EUR')
+        self.assertEqual(twentyEuros, tenEuros.times(2))
+    
+    def testDivision(self):
+        originalMoney = Money(4002, 'KRW')
+        dividedMoney = Money(1000.5, 'KRW')
+        self.assertEqual(dividedMoney, originalMoney.divide(4))
 
 if __name__ == '__main__':
     unittest.main()
