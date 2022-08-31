14e85be73eff7f367b0833de6dc7eadfc0110baa
teacher144123 <teacher144123@gmail.com>
2022-08-31 10:30:02 +0800

    feat: convert from EUR to USD

--- go/stocks/portfolio.go
+++ go/stocks/portfolio.go
@@ -14,7 +14,16 @@ func (p Portfolio) Add(money Money) Portfolio {
 func (p Portfolio) Evaluate(currency string) Money {
 	total := 0.0
 	for _, m := range p {
-		total += m.amount
+		total += convert(m, currency)
 	}
 	return Money{amount: total, currency: currency}
+}
+
+func convert(m Money, currency string) float64 {
+	const EURToUSD float64 = 1.2
+	if m.currency == currency {
+		return m.amount
+	} else {
+		return m.amount * EURToUSD
+	}
 }
--- py/portfolio.py
+++ py/portfolio.py
@@ -6,6 +6,7 @@ from money import Money
 class Portfolio:
     def __init__(self):
         self.moneys = []
+        self._EURToUSD = 1.2
     
     def add(self, *moneys):
         self.moneys.extend(moneys)
@@ -16,7 +17,13 @@ class Portfolio:
             total += m.amount
         total = functools.reduce(
             operator.add,
-            [m.amount for m in self.moneys],
+            [self.__convert(m, currency) for m in self.moneys],
             0
         )
         return Money(total, currency)
+    
+    def __convert(self, money, currency):
+        if money.currency == currency:
+            return money.amount
+        else:
+            return money.amount * self._EURToUSD
